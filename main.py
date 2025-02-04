91b96bab8ef738f951825b08ea83430bc3dd589a
mailtoajay7007 <mailtoajay7007@gmail.com>
2025-02-04 18:47:39 +0000

    question2

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 # Suggested code may be subject to a license. Learn more: ~LicenseLog:2624359296.
   # 1. perfect square 
-def isPerfectSquare(num):
-    if num < 0:
-        return False
-    if num == 0:
-      return True
-    x = int(num**0.5)
-    return x * x == num
+# def isPerfectSquare(num):
+#     if num < 0:
+#         return False
+#     if num == 0:
+#       return True
+#     x = int(num**0.5)
+#     return x * x == num
             
-print(isPerfectSquare(1))
+# print(isPerfectSquare(1))
 
-        
+# 2. print the fractions
+
+def add_digits(int):
+    while num >= 10:
+        sum_digits = 0
+        for digit in str(num):
+            sum_digits += int(digit)
+        num = sum_digits
+    return num
+
