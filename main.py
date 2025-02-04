d4a018c2ba96c272cfb10e6817d6838ef1c351ac
mailtoajay7007 <mailtoajay7007@gmail.com>
2025-02-04 18:10:48 +0000

    comments removed

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 # Suggested code may be subject to a license. Learn more: ~LicenseLog:2624359296.
   # 1. perfect square 
 def isPerfectSquare(num):
-    """
-    Checks if a number is a perfect square.
-    
-    Args:
-      num: The number to check.
-    
-    Returns:
-      True if the number is a perfect square, False otherwise.
-    """
     if num < 0:
         return False
     if num == 0:
