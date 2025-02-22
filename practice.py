72e8c58a87e441e9d7923d95fff655701bbeff25
mailtoajay7007 <mailtoajay7007@gmail.com>
2025-02-22 16:47:44 +0000

    prime number

--- practice.py
+++ practice.py
@@ -1,11 +1,25 @@
-y=2016
-population = 80000
-for i in range(y,2021):
+# y=2016
+# population = 80000
+# for i in range(y,2021):
 
-    if y%2==0:
-        population+= population*(10/100)
-        y+=1
-    else:
-        population-= population*(10/100)
-        y+=1
-print(population)
+#     if y%2==0:
+#         population+= population*(10/100)
+#         y+=1
+#     else:
+#         population-= population*(10/100)
+#         y+=1
+# print(population)
+
+
+
+# class Solution:
+#     def isPrime(self, n):
+#         # code here
+#         if n<=3:
+#             return True
+#         else:
+#             for i in range(1,n):
+#                 if n%i==0 and (i!=1 and i!=n):
+#                     return False
+#             return True
+#         return True
